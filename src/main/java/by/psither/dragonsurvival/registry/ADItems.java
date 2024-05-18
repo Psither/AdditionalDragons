b6cc20e90c68cf7ee8decf6cc1f11576eb897294
Psither <97207971+Psither@users.noreply.github.com>
2024-05-18 02:15:06 -0400

    Update ADItems.java

--- src/main/java/by/psither/dragonsurvival/registry/ADItems.java
+++ src/main/java/by/psither/dragonsurvival/registry/ADItems.java
@@ -13,7 +13,6 @@ import by.dragonsurvivalteam.dragonsurvival.common.items.food.DragonFoodItem;
 import by.dragonsurvivalteam.dragonsurvival.util.Functions;
 import by.psither.dragonsurvival.AdditionalDragonsMod;
 import by.psither.dragonsurvival.common.items.AncientCatalystItem;
-import net.minecraft.Util;
 import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -28,13 +27,13 @@ import net.minecraft.world.item.Item.Properties;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.registries.RegisterEvent;
 
 @Mod.EventBusSubscriber( modid = AdditionalDragonsMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD )
 public class ADItems {
 	public static HashMap<String, Item> AD_ITEMS = new HashMap<>();
 	public static Item slipperySushi, cursedMarrow;
+	//public static Item revolvingHearts;
 	public static Item ancientCatalyst;
 	
 	@SubscribeEvent
@@ -45,6 +44,7 @@ public class ADItems {
 		slipperySushi = registerItem(event, new DragonFoodItem(defaultProperties, DragonTypes.SEA, () -> new MobEffectInstance(MobEffects.DOLPHINS_GRACE, Functions.secondsToTicks(20))), "slippery_sushi");
 		cursedMarrow = registerItem(event, new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(4).saturationMod(0.8f).alwaysEat().fast().effect(new MobEffectInstance(ADDragonEffects.CONFOUNDED, Functions.secondsToTicks(20), 1), 1f).build())), "cursed_marrow");
 		ancientCatalyst = registerItem(event, new AncientCatalystItem(defaultProperties.stacksTo(1).rarity(Rarity.EPIC)), "ancient_catalyst");
+		//revolvingHearts = registerItem(event, new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(4).saturationMod(0.8f).alwaysEat().fast().build())), "revolving_hearts");
 	}
 
 	public static Item registerItem(RegisterEvent event, String name, String description){
