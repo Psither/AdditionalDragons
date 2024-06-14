75b78dc32990a3a2d02e68c25e8b521126af7d44
Psither <97207971+Psither@users.noreply.github.com>
2024-06-13 21:54:24 -0400

    updates

--- src/main/java/by/psither/dragonsurvival/commands/DragonSubtypeSwapCommand.java
+++ src/main/java/by/psither/dragonsurvival/commands/DragonSubtypeSwapCommand.java
@@ -66,7 +66,7 @@ public class DragonSubtypeSwapCommand {
 			NetworkHandler.CHANNEL.send(PacketDistributor.TRACKING_ENTITY_AND_SELF.with(() -> serverplayer),new SynchronizeDragonCap(serverplayer.getId(), cap.isHiding(), cap.getType(), cap.getSize(), cap.hasWings(), 0));
 			NetworkHandler.CHANNEL.send(PacketDistributor.TRACKING_ENTITY_AND_SELF.with(() -> serverplayer),new SyncSpinStatus(serverplayer.getId(), cap.getMovementData().spinAttack, cap.getMovementData().spinCooldown, cap.getMovementData().spinLearned));
 			NetworkHandler.CHANNEL.send(PacketDistributor.TRACKING_ENTITY_AND_SELF.with(() -> serverplayer), new SyncSize(serverplayer.getId(), cap.getSize()));
-			NetworkHandler.CHANNEL.send(PacketDistributor.PLAYER.with(() -> serverplayer), new RequestClientData(cap.getType(), cap.getLevel()));
+			NetworkHandler.CHANNEL.send(PacketDistributor.PLAYER.with(() -> serverplayer), new RequestClientData(cap.getType(), cap.getBody(), cap.getLevel()));
 			serverplayer.refreshDimensions();
 
 			//pSource.sendSuccess(Component.translatable("ds.commands.ability_swap.success"), true);
