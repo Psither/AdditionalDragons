177759b0d47bd6c54b489092a65974d2db6c0f7f
Psither <97207971+Psither@users.noreply.github.com>
2024-10-14 16:41:02 -0400

    Increased cooldown on food generation

--- src/main/java/by/psither/dragonsurvival/config/ADServerConfig.java
+++ src/main/java/by/psither/dragonsurvival/config/ADServerConfig.java
@@ -1,7 +1,7 @@
 package by.psither.dragonsurvival.config;
 
 public class ADServerConfig {
-    public static final Integer ASTRAL_FOOD_TICK_FREQUENCY = 150;
+    public static final Integer ASTRAL_FOOD_TICK_FREQUENCY = 800;
     public static final Double VOID_BODY_DRAIN = 0.05;
     public static final Double VOID_BODY_FOOD_DRAIN = 0.01;
 }
