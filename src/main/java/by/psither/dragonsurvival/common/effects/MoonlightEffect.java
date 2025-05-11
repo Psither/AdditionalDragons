c32131eb73e3eaae49eaa6b1c5b86dd6465a1c66
Psither <97207971+Psither@users.noreply.github.com>
2025-05-11 16:36:29 -0400

    Renamed InvigorateEffect to MoonlightEffect

--- src/main/java/by/psither/dragonsurvival/common/effects/MoonlightEffect.java
+++ src/main/java/by/psither/dragonsurvival/common/effects/MoonlightEffect.java
@@ -6,8 +6,8 @@ import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
 import org.jetbrains.annotations.NotNull;
 
-public class InvigorateEffect extends ModifiableMobEffect {
-    public InvigorateEffect(MobEffectCategory type, int color, boolean incurable) {
+public class MoonlightEffect extends ModifiableMobEffect {
+    public MoonlightEffect(MobEffectCategory type, int color, boolean incurable) {
         super(type, color, incurable);
     }
 
--- src/main/java/by/psither/dragonsurvival/registry/ADEffects.java
+++ src/main/java/by/psither/dragonsurvival/registry/ADEffects.java
@@ -4,7 +4,7 @@ import by.dragonsurvivalteam.dragonsurvival.common.effects.ConfoundedEffect;
 import by.psither.dragonsurvival.AdditionalDragonsMod;
 import by.psither.dragonsurvival.common.effects.BubbleShieldEffect;
 import by.psither.dragonsurvival.common.effects.HighVoltageEffect;
-import by.psither.dragonsurvival.common.effects.InvigorateEffect;
+import by.psither.dragonsurvival.common.effects.MoonlightEffect;
 import by.psither.dragonsurvival.common.effects.SeekingTalonsEffect;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -22,7 +22,7 @@ public class ADEffects {
     public static Holder<MobEffect> HIGH_VOLTAGE = REGISTRY.register("high_voltage", () -> new HighVoltageEffect(MobEffectCategory.BENEFICIAL, 0x0, false));
     public static Holder<MobEffect> BUBBLE_SHIELD = REGISTRY.register("bubble_shield", () -> new BubbleShieldEffect(MobEffectCategory.BENEFICIAL, 0x0)
             .addAttributeModifier(Attributes.MAX_ABSORPTION, res("effect.bubble_shield.absorption"), 2.0, AttributeModifier.Operation.ADD_VALUE));
-    public static Holder<MobEffect> INVIGORATE = REGISTRY.register("invigorate", () -> new InvigorateEffect(MobEffectCategory.BENEFICIAL, 0x0, false)
+    public static Holder<MobEffect> INVIGORATE = REGISTRY.register("invigorate", () -> new MoonlightEffect(MobEffectCategory.BENEFICIAL, 0x0, false)
             .addAttributeModifier(Attributes.MOVEMENT_SPEED, res("effect.invigorate.movement_speed"), 0.1, AttributeModifier.Operation.ADD_MULTIPLIED_BASE)
             .addAttributeModifier(Attributes.ATTACK_SPEED, res("effect.invigorate.attack_speed"), 0.2, AttributeModifier.Operation.ADD_MULTIPLIED_BASE)
             .addAttributeModifier(Attributes.LUCK, res("effect.invigorate.luck"), 0.5, AttributeModifier.Operation.ADD_VALUE)
