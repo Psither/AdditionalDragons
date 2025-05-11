8b8be3dc43fe6c01f8b65680e0036d025019f7c4
Psither <97207971+Psither@users.noreply.github.com>
2025-05-11 15:54:34 -0400

    update comment

--- src/main/java/by/psither/dragonsurvival/common/effects/InvigorateEffect.java
+++ src/main/java/by/psither/dragonsurvival/common/effects/InvigorateEffect.java
@@ -14,7 +14,7 @@ public class InvigorateEffect extends ModifiableMobEffect {
     @Override
     public boolean applyEffectTick(@NotNull LivingEntity livingEntity, int amplifier) {
         float daytime = livingEntity.level().getTimeOfDay(0);
-        if (CustomPredicates.getSunLightLevel(livingEntity) > 4 && (daytime > 12000 || daytime < 1000)) { // In light & the moon is out
+        if (CustomPredicates.getSunLightLevel(livingEntity) > 4 && (daytime > 12000 || daytime < 1000)) { // In skylight & the moon is out
             this.addAttributeModifiers(livingEntity.getAttributes(), amplifier);
         } else {
             this.removeAttributeModifiers(livingEntity.getAttributes());
